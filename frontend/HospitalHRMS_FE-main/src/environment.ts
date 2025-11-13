30f08923e67e76eddae5d9e579cb9aae1e9ef2f6
Gokul-S2477 <saigokul2477@gmail.com>
2025-11-13 17:36:41 +0530

    updated files

--- frontend/HospitalHRMS_FE-main/src/environment.ts
+++ frontend/HospitalHRMS_FE-main/src/environment.ts
@@ -1,8 +1,9 @@
 // src/environment.ts
 
 // ✅ Base API URL (Hardcoded for local development)
-export const API_BASE = "http://127.0.0.1:8000";
 
+//export const API_BASE = process.env.REACT_APP_API_BASE || "http://127.0.0.1:8000";
+export const API_BASE = "http://127.0.0.1:8000";
 // 🌄 Image path
 export const img_path = "/assets/img/";
 
