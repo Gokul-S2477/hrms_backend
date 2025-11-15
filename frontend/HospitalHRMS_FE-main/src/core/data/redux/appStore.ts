d5f840aa68a9d4a1436b5d790829625f360b9678
Gokul-S2477 <saigokul2477@gmail.com>
2025-11-15 13:03:12 +0530

    frontend update

--- frontend/HospitalHRMS_FE-main/src/core/data/redux/appStore.ts
+++ frontend/HospitalHRMS_FE-main/src/core/data/redux/appStore.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
+import themeSettingReducer from "./themeSettingSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    themeSetting: themeSettingReducer,
   },
 });
 
 export default store;
+
+
